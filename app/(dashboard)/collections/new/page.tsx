c6fdd4e010178cabe20b9c5458ad22f960b27ee5
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-10-19 12:02:25 +0700

    add shadc ui and config

---
+++ app/(dashboard)/collections/new/page.tsx
@@ -0,0 +1,10 @@
+import CollectionForm from '@/components/collections/CollectionForm'
+import React from 'react'
+
+const page = () => {
+    return (
+        <CollectionForm />
+    )
+}
+
+export default page
---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Separator } from '../ui/separator'
+
+const CollectionForm = () => {
+    return (
+        <div className='p-10'>
+            <p className='text-heading1-bold'>Create Collection</p>
+            <Separator className='my-4'/>
+        </div>
+
+    )
+}
+
+export default CollectionForm
