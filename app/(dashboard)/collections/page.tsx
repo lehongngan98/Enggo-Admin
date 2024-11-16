1cc353831774488d820c994d96ea921c60bdd5ee
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-11-16 14:15:29 +0700

    add search collections

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { columns } from "@/components/collections/CollectionColumn";
 import { DataTable } from "@/components/custom ui/DataTable";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Collections = () => {
@@ -31,12 +34,21 @@ const Collections = () => {
     console.log(collections);
 
     return (
-        <div>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <DataTable columns={columns} data={collections} />
-            )}
+        <div className="px-10 py-5">
+            <div className="flex items-center justify-between ">
+                <p className="text-heading3-bold">Collections</p>
+                <Button
+                    className="ml-2"
+                    onClick={() => {
+                        console.log("Create Collection");
+                    }}
+                >
+                    <Plus className="h-4 w-4 mr-2" />
+                    Create Collection
+                </Button>
+            </div>
+            <Separator className="bg-grey-1 my-4" />
+            <DataTable columns={columns} data={collections} searchKey="title"/>
         </div>
     );
 };
--- components/custom ui/DataTable.tsx
+++ components/custom ui/DataTable.tsx
@@ -1,80 +1,122 @@
-"use client"
+"use client";
 
 import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table"
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Input } from "../ui/input";
+import React from "react";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+	searchKey: string;
 }
 
 export function DataTable<TData, TValue>({
-  columns,
-  data,
-}: DataTableProps<TData, TValue>) {
-  const table = useReactTable({
-    data,
     columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
+    data,
+	searchKey,
+}: DataTableProps<TData, TValue>) {
+    const [columnFilters, setColumnFilters] =
+        React.useState<ColumnFiltersState>([]);
 
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            columnFilters,
+        },
+    });
+
+    return (
+        <div className="py-5">
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Search..."
+                    value={
+                        (table
+                            .getColumn(searchKey)
+                            ?.getFilterValue() as string) ?? ""
+                    }
+                    onChange={(event) =>
+                        table
+                            .getColumn(searchKey)
+                            ?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
+
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                      header.column.columnDef
+                                                          .header,
+                                                      header.getContext()
+                                                  )}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={
+                                        row.getIsSelected() && "selected"
+                                    }
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
                         )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  )
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    );
 }
