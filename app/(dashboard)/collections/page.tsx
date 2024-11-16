d6df3efddab504906b57c92ecbfdcd4eb7e0d993
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-11-16 13:21:39 +0700

    render api collections

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import { columns } from "@/components/collections/CollectionColumn";
+import { DataTable } from "@/components/custom ui/DataTable";
+import { useEffect, useState } from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const Collections = () => {
+    const [loading, setLoading] = useState(true);
+    const [collections, setCollections] = useState([]);
 
-export default page
+    const getCollections = async () => {
+        try {
+            const res = await fetch("/api/collections", {
+                method: "GET",
+            });
+            if (!res.ok) {
+                throw new Error("Failed to fetch collections");
+            }
+            const data = await res.json();
+            setCollections(data);
+            setLoading(false);
+        } catch (error) {
+            console.error("[collections_GET]", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        getCollections();
+    }, []);
+
+    console.log(collections);
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <DataTable columns={columns} data={collections} />
+            )}
+        </div>
+    );
+};
+
+export default Collections;
