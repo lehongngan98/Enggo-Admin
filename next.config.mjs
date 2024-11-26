0db3d3b9dc469aa71b1b426a1c02cc7b1fda2d56
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-11-26 15:18:27 +0700

    fix error config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['res.cloudinary.com']
-    }
+    },
+    experimental: {
+        runtime: 'edge',
+    },
 };
 
 export default nextConfig;
